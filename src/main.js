6fe7c4971a510d00520663f74b579c5e1fe7a62c
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2024-12-25 17:58:53 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,61 @@
+import './style.css'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+
+let aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#main-canvas'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.set( 0, 15, 25 );
+
+renderer.render( scene, camera );
+
+const loader = new GLTFLoader();
+let model;
+
+loader.load( "model/maxwell.gltf", 
+  (gltf) => {
+    model = gltf.scene;
+    const scale = 0.5;
+    model.scale.set( scale, scale, scale );
+    scene.add( model );
+  },
+  (xhr) => {
+    console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+  },
+  (err) => {
+    console.log( 'error while loading gltf: ', err);
+  }
+);
+
+const light = new THREE.AmbientLight( 0xffffff, 5 );
+scene.add(light);
+
+const orbitalControls = new OrbitControls( camera, renderer.domElement );
+
+function animate() {
+  requestAnimationFrame( animate );
+  
+  if (model) model.rotation.y -= 0.007;
+  orbitalControls.update();
+
+  renderer.render( scene, camera );
+}
+
+window.addEventListener( 'resize', () => {
+  aspect = window.innerWidth / window.innerHeight;
+  camera.aspect = aspect;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+} );
+
+animate();
+
