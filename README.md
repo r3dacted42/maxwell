d6565fa047a79eb6515e8654f2c7d7b828c00f9d
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2024-12-27 03:16:42 +0530

    added skybox

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Maxwell the Cat (dingus)  
 
 ![deployment](https://github.com/r3dacted42/maxwell/workflows/CI/CD/badge.svg)  
-appreciation page for maxwell the cat, aka dingus meme.  
+appreciation page for maxwell the cat, aka dingus.  
 
-model by: [bean](https://steamcommunity.com/sharedfiles/filedetails/?id=2874462324)  
-music by: [Mr. Weebl](https://youtu.be/3W-EkANZX8g)  
+model by: [bean (steam)](https://steamcommunity.com/sharedfiles/filedetails/?id=2874462324) | [bean (sketchfab)](https://sketchfab.com/3d-models/maxwell-the-cat-dingus-2ca7f3c1957847d6a145fc35de9046b0)  
+background by: [Alexander (steam)](https://steamcommunity.com/sharedfiles/filedetails/?id=2475844874)
+music by: [Mr. Weebl (youtube)](https://youtu.be/3W-EkANZX8g)  
 
 all credits go to respective owners.
--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 
 let aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 20000);
 
 const renderer = new THREE.WebGLRenderer({
+  antialias: true,
   canvas: $('#main-canvas')[0],
 });
 
@@ -18,22 +19,39 @@ camera.position.set(0, 7, 15);
 
 renderer.render(scene, camera);
 
-const loader = new GLTFLoader();
+new THREE.TextureLoader().load("image/gm_construct.png",
+  (texture) => {
+    const skySphereGeometry = new THREE.SphereGeometry(1000, 100, 100);
+    const skySphereMaterial = new THREE.MeshBasicMaterial({
+      map: texture,
+    });
+    skySphereMaterial.side = THREE.BackSide;
+    const skySphereMesh = new THREE.Mesh(skySphereGeometry, skySphereMaterial);
+    scene.add(skySphereMesh);
+  },
+  (xhr) => {
+    console.log('texture ' + (xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  (err) => {
+    console.log('error while loading texture: ', err);
+  }
+);
+
 let model;
 
-loader.load("model/maxwell.gltf",
-  (gltf) => {
-    model = gltf.scene;
+new GLTFLoader().load("model/maxwell.gltf",
+  (dingus) => {
+    model = dingus.scene;
     const scale = 0.3;
     model.scale.set(scale, scale, scale);
     model.position.set(0, -3, 0);
     scene.add(model);
   },
   (xhr) => {
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    console.log('dingus ' + (xhr.loaded / xhr.total * 100) + '% loaded');
   },
   (err) => {
-    console.log('error while loading gltf: ', err);
+    console.log('error while loading dingus: ', err);
   }
 );
 
@@ -43,12 +61,12 @@ scene.add(light);
 const orbitalControls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
-  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 
-  if (model) model.rotation.y -= 0.007;
+  if (model) model.rotation.y -= 0.01;
   orbitalControls.update();
 
-  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
 
 $(window).resize(() => {
@@ -60,10 +78,10 @@ $(window).resize(() => {
 
 animate();
 
-$(document).on('visibilitychange pointerdown', () => {
-  if (document.visibilityState === 'hidden') {
-    $('#bgm')[0].pause();
-  } else {
-    $('#bgm')[0].play();
-  }
+$(document).on('pointerdown', () => {
+  $('#bgm')[0].play();
+});
+
+$(document).on('visibilitychange', () => {
+  if (document.visibilityState === 'hidden') $('#bgm')[0].pause();
 });
